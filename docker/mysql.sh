5f95c24b95601e9a0f317d661d579339d9e5e4ff
Jose Riguera <jriguera@gmail.com>
2019-11-01 20:00:31 +0100

    improve script

--- docker/mysql.sh
+++ docker/mysql.sh
@@ -3,16 +3,18 @@ set -eo pipefail
 shopt -s nullglob
 
 # if command starts with an option, prepend mysqld
-if [ "${1:0:1}" = '-' ]; then
+if [ "${1:0:1}" == '-' ]
+then
 	set -- mysqld "$@"
 fi
 
 # skip setup if they want an option that stops mysqld
-wantHelp=
-for arg; do
+HELP=0
+for arg
+do
 	case "$arg" in
 		-'?'|--help|--print-defaults|-V|--version)
-			wantHelp=1
+			HELP=1
 			break
 			;;
 	esac
@@ -23,29 +25,33 @@ done
 # (will allow for "$XYZ_DB_PASSWORD_FILE" to fill in the value of
 #  "$XYZ_DB_PASSWORD" from a file, especially for Docker's secrets feature)
 file_env() {
-	local var="$1"
-	local fileVar="${var}_FILE"
+	local var="${1}"
 	local def="${2:-}"
-	if [ "${!var:-}" ] && [ "${!fileVar:-}" ]; then
-		echo >&2 "error: both $var and $fileVar are set (but are exclusive)"
-		exit 1
+
+	local fvar="${var}_FILE"
+	local val="${def}"
+
+	if [ -n "${!var:-}" ] && [ -r "${fvar}" ]
+	then
+		echo "* Warning: both ${var} and ${fvar} are set, env ${var} takes priority"
 	fi
-	local val="$def"
-	if [ "${!var:-}" ]; then
+	if [ -n "${!var:-}" ]
+	then
 		val="${!var}"
-	elif [ "${!fileVar:-}" ]; then
-		val="$(< "${!fileVar}")"
+	elif [ -r "${fvar}" ]
+	then
+		val=$(< "${fvar}")
 	fi
-	export "$var"="$val"
-	unset "$fileVar"
+	export "${var}"="${val}"
 }
 
 _check_config() {
-	toRun=( "$@" --verbose --help --log-bin-index="$(mktemp -u)" )
-	if ! errors="$("${toRun[@]}" 2>&1 >/dev/null)"; then
+	local run=( "$@" --verbose --help --log-bin-index="$(mktemp -u)" )
+	if ! errors="$("${run[@]}" 2>&1 >/dev/null)"
+	then
 		cat >&2 <<-EOM
 			ERROR: mysqld failed while attempting to check config
-			command was: "${toRun[*]}"
+			command was: "${run[*]}"
 			$errors
 		EOM
 		exit 1
@@ -56,12 +62,15 @@ _check_config() {
 # We use mysqld --verbose --help instead of my_print_defaults because the
 # latter only show values present in config files, and not server defaults
 _get_config() {
-	local conf="$1"; shift
-	"$@" --verbose --help --log-bin-index="$(mktemp -u)" 2>/dev/null | awk '$1 == "'"$conf"'" { print $2; exit }'
+	local conf="${1}"
+	shift
+
+	"$@" --verbose --help --log-bin-index="$(mktemp -u)" 2>/dev/null | awk '$1 == "'"${conf}"'" { print $2; exit }'
 }
 
 # allow the container to be started with `--user`
-if [ "$1" = 'mysqld' -a -z "$wantHelp" -a "$(id -u)" = '0' ]; then
+if [ "${1}" == "mysqld" -a "${HELP}" == "0" -a "$(id -u)" == "0" ]
+then
 	_check_config "$@"
 	DATADIR="$(_get_config 'datadir' "$@")"
 	mkdir -p "$DATADIR"
@@ -69,61 +78,66 @@ if [ "$1" = 'mysqld' -a -z "$wantHelp" -a "$(id -u)" = '0' ]; then
 	exec su-exec mysql "$BASH_SOURCE" "$@"
 fi
 
-if [ "$1" = 'mysqld' -a -z "$wantHelp" ]; then
+if [ "${1}" == 'mysqld' -a "${HELP}" == "0" ]
+then
 	# still need to check config, container may have started with --user
 	_check_config "$@"
 	# Get config
 	DATADIR="$(_get_config 'datadir' "$@")"
-
-	if [ ! -d "$DATADIR/mysql" ]; then
+	if [ ! -d "$DATADIR/mysql" ]
+	then
 		file_env 'MYSQL_ROOT_PASSWORD'
-		if [ -z "$MYSQL_ROOT_PASSWORD" -a -z "$MYSQL_ALLOW_EMPTY_PASSWORD" -a -z "$MYSQL_RANDOM_ROOT_PASSWORD" ]; then
-			echo >&2 'error: database is uninitialized and password option is not specified '
-			echo >&2 '  You need to specify one of MYSQL_ROOT_PASSWORD, MYSQL_ALLOW_EMPTY_PASSWORD and MYSQL_RANDOM_ROOT_PASSWORD'
+		if [ -z "$MYSQL_ROOT_PASSWORD" -a -z "$MYSQL_ALLOW_EMPTY_PASSWORD" -a -z "$MYSQL_RANDOM_ROOT_PASSWORD" ]
+		then
+			echo >&2 'ERROR: database is uninitialized and password option is not specified '
+			echo >&2 '       You need to specify one of MYSQL_ROOT_PASSWORD, MYSQL_ALLOW_EMPTY_PASSWORD and MYSQL_RANDOM_ROOT_PASSWORD'
 			exit 1
 		fi
 
+		echo "* Initializing $DATADIR ..."
 		mkdir -p "$DATADIR"
-
-		echo 'Initializing database'
-		mysql_install_db --datadir="$DATADIR" --rpm
-		echo 'Database initialized'
+		mysql_install_db --skip-test-db --datadir="$DATADIR" > /dev/null
 
 		SOCKET="$(_get_config 'socket' "$@")"
-		"$@" --skip-networking --socket="${SOCKET}" &
-		pid="$!"
+		"$@" --silent-startup --skip-networking --socket="${SOCKET}" &
+		pid=$!
 
 		mysql=( mysql --protocol=socket -uroot -hlocalhost --socket="${SOCKET}" )
-
-		for i in {30..0}; do
-			if echo 'SELECT 1' | "${mysql[@]}" &> /dev/null; then
+		for i in {30..0}
+		do
+			if echo 'SELECT 1' | "${mysql[@]}" &> /dev/null
+			then
 				break
 			fi
-			echo 'MySQL init process in progress...'
+			echo '* MySQL init process in progress ...'
 			sleep 1
 		done
-		if [ "$i" = 0 ]; then
-			echo >&2 'MySQL init process failed.'
+		if [ "$i" == 0 ]
+		then
+			echo >&2 '* MySQL init process FAILED.'
 			exit 1
 		fi
 
-		if [ -z "$MYSQL_INITDB_SKIP_TZINFO" ]; then
+		if [ -z "$MYSQL_INITDB_SKIP_TZINFO" ]
+		then
 			# sed is for https://bugs.mysql.com/bug.php?id=20545
 			mysql_tzinfo_to_sql /usr/share/zoneinfo | sed 's/Local time zone must be set--see zic manual page/FCTY/' | "${mysql[@]}" mysql
 		fi
 
-		if [ ! -z "$MYSQL_RANDOM_ROOT_PASSWORD" ]; then
+		if [ ! -z "$MYSQL_RANDOM_ROOT_PASSWORD" ]
+		then
 			export MYSQL_ROOT_PASSWORD="$(pwgen -1 32)"
-			echo "GENERATED ROOT PASSWORD: $MYSQL_ROOT_PASSWORD"
+			echo "* Generated ROOT password: $MYSQL_ROOT_PASSWORD"
 		fi
 
-		rootCreate=
+		rootgrants=""
 		# default root to listen for connections from anywhere
 		file_env 'MYSQL_ROOT_HOST' '%'
-		if [ ! -z "$MYSQL_ROOT_HOST" -a "$MYSQL_ROOT_HOST" != 'localhost' ]; then
+		if [ ! -z "$MYSQL_ROOT_HOST" -a "$MYSQL_ROOT_HOST" != 'localhost' ]
+		then
 			# no, we don't care if read finds a terminating character in this heredoc
 			# https://unix.stackexchange.com/questions/265149/why-is-set-o-errexit-breaking-this-read-heredoc-expression/265151#265151
-			read -r -d '' rootCreate <<-EOSQL || true
+			read -r -d '' rootgrants <<-EOSQL || true
 				CREATE USER 'root'@'${MYSQL_ROOT_HOST}' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}' ;
 				GRANT ALL ON *.* TO 'root'@'${MYSQL_ROOT_HOST}' WITH GRANT OPTION ;
 			EOSQL
@@ -136,51 +150,64 @@ if [ "$1" = 'mysqld' -a -z "$wantHelp" ]; then
 			DELETE FROM mysql.user WHERE user NOT IN ('mysql.sys', 'mysqlxsys', 'root') OR host NOT IN ('localhost') ;
 			SET PASSWORD FOR 'root'@'localhost'=PASSWORD('${MYSQL_ROOT_PASSWORD}') ;
 			GRANT ALL ON *.* TO 'root'@'localhost' WITH GRANT OPTION ;
-			${rootCreate}
+			${rootgrants}
 			DROP DATABASE IF EXISTS test ;
 			FLUSH PRIVILEGES ;
 		EOSQL
 
-		if [ ! -z "$MYSQL_ROOT_PASSWORD" ]; then
+		if [ ! -z "$MYSQL_ROOT_PASSWORD" ]
+		then
 			mysql+=( -p"${MYSQL_ROOT_PASSWORD}" )
 		fi
 
 		file_env 'MYSQL_DATABASE'
-		if [ "$MYSQL_DATABASE" ]; then
+		if [ "$MYSQL_DATABASE" ]
+		then
+			echo "* Generating database $MYSQL_DATABASE if not exists ..."
 			echo "CREATE DATABASE IF NOT EXISTS \`$MYSQL_DATABASE\` ;" | "${mysql[@]}"
 			mysql+=( "$MYSQL_DATABASE" )
 		fi
 
 		file_env 'MYSQL_USER'
 		file_env 'MYSQL_PASSWORD'
-		if [ "$MYSQL_USER" -a "$MYSQL_PASSWORD" ]; then
+		if [ "$MYSQL_USER" -a "$MYSQL_PASSWORD" ]
+		then
+			echo "* Creating user $MYSQL_USER and grant access to database $MYSQL_DATABASE ..."
 			echo "CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD' ;" | "${mysql[@]}"
-
-			if [ "$MYSQL_DATABASE" ]; then
-				echo "GRANT ALL ON \`$MYSQL_DATABASE\`.* TO '$MYSQL_USER'@'%' ;" | "${mysql[@]}"
-			fi
-
+			[ -n "$MYSQL_DATABASE" ] && echo "GRANT ALL ON \`$MYSQL_DATABASE\`.* TO '$MYSQL_USER'@'%' ;" | "${mysql[@]}"
 			echo 'FLUSH PRIVILEGES ;' | "${mysql[@]}"
 		fi
 
-		echo
-		for f in /docker-entrypoint-initdb.d/*; do
-			case "$f" in
-				*.sh)     echo "$0: running $f"; . "$f" ;;
-				*.sql)    echo "$0: running $f"; "${mysql[@]}" < "$f"; echo ;;
-				*.sql.gz) echo "$0: running $f"; gunzip -c "$f" | "${mysql[@]}"; echo ;;
-				*)        echo "$0: ignoring $f" ;;
+		for f in /docker-entrypoint-initdb.d/*
+		do
+			case "${f}" in
+				*.sh)
+					echo "${0}: Running ${f} ..."
+					( . "${f}" )
+				;;
+				*.sql)
+					echo "${0}: Loading SQL from ${f} ..."
+					"${mysql[@]}" < "${f}"
+					echo
+				;;
+				*.sql.gz)
+					echo "${0}: Loading gziped SQL from ${f} ..."
+					gunzip -c "${f}" | "${mysql[@]}"
+					echo
+				;;
+				*)
+					echo "${0}: Ignoring ${f} ..."
+				;;
 			esac
-			echo
 		done
 
-		if ! kill -s TERM "$pid" || ! wait "$pid"; then
-			echo >&2 'MySQL init process failed.'
+		if ! kill -s TERM ${pid} || ! wait ${pid}
+		then
+			echo >&2 '* MySQL init process FAILED.'
 			exit 1
 		fi
 
-		echo
-		echo 'MySQL init process done. Ready for start up.'
+		echo '* MySQL init process DONE. Ready for start up.'
 		echo
 	fi
 fi
