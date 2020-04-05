a6003705c54afaca053e3300f829907b0ba6a62f
Jose Riguera <jriguera@gmail.com>
2020-04-05 14:10:16 +0200

    lot of improvements: listent to kill signals, be able to create different databases

--- docker/mysql.sh
+++ docker/mysql.sh
@@ -5,7 +5,8 @@ shopt -s nullglob
 # if command starts with an option, prepend mysqld
 if [ "${1:0:1}" == '-' ]
 then
-	set -- mysqld "$@"
+	# Listen to signals, most importantly CTRL+C
+	set -- mysqld --debug-gdb "$@"
 fi
 
 # skip setup if they want an option that stops mysqld
@@ -45,7 +46,7 @@ file_env() {
 	export "${var}"="${val}"
 }
 
-_check_config() {
+check_config() {
 	local run=( "$@" --verbose --help --log-bin-index="$(mktemp -u)" )
 	if ! errors="$("${run[@]}" 2>&1 >/dev/null)"
 	then
@@ -61,48 +62,40 @@ _check_config() {
 # Fetch value from server config
 # We use mysqld --verbose --help instead of my_print_defaults because the
 # latter only show values present in config files, and not server defaults
-_get_config() {
+get_config() {
 	local conf="${1}"
 	shift
-
-	"$@" --verbose --help --log-bin-index="$(mktemp -u)" 2>/dev/null | awk '$1 == "'"${conf}"'" { print $2; exit }'
+	"$@" --verbose --help --log-bin-index="$(mktemp -u)" 2>/dev/null | awk -v var="^${conf} " '{ if ($0 ~ var) { print $2; exit }}'
 }
 
 # allow the container to be started with `--user`
 if [ "${1}" == "mysqld" -a "${HELP}" == "0" -a "$(id -u)" == "0" ]
 then
-	_check_config "$@"
-	DATADIR="$(_get_config 'datadir' "$@")"
-	mkdir -p "$DATADIR"
-	chown -R mysql:mysql "$DATADIR"
-	exec su-exec mysql "$BASH_SOURCE" "$@"
+	check_config "$@"
+	DATADIR="$(get_config 'datadir' "$@")"
+	mkdir -p "${DATADIR}"
+	chown -R mysql:mysql "${DATADIR}"
+	exec su-exec mysql "${BASH_SOURCE}" "$@"
 fi
 
 if [ "${1}" == 'mysqld' -a "${HELP}" == "0" ]
 then
 	# still need to check config, container may have started with --user
-	_check_config "$@"
+	check_config "$@"
 	# Get config
-	DATADIR="$(_get_config 'datadir' "$@")"
-	if [ ! -d "$DATADIR/mysql" ]
+	SOCKET="$(get_config 'socket' "$@")"
+	mysql=( mysql --protocol=socket -uroot -hlocalhost --socket="${SOCKET}" )
+	DATADIR="$(get_config 'datadir' "$@")"
+	if [ ! -d "${DATADIR}/mysql" ]
 	then
-		file_env 'MYSQL_ROOT_PASSWORD'
-		if [ -z "$MYSQL_ROOT_PASSWORD" -a -z "$MYSQL_ALLOW_EMPTY_PASSWORD" -a -z "$MYSQL_RANDOM_ROOT_PASSWORD" ]
-		then
-			echo >&2 'ERROR: database is uninitialized and password option is not specified '
-			echo >&2 '       You need to specify one of MYSQL_ROOT_PASSWORD, MYSQL_ALLOW_EMPTY_PASSWORD and MYSQL_RANDOM_ROOT_PASSWORD'
-			exit 1
-		fi
+		file_env 'MYSQL_ROOT_PASSWORD' "$(pwgen -1 32)"
+		echo "${MYSQL_ROOT_PASSWORD}" > "${DATADIR}/root.password"
 
-		echo "* Initializing $DATADIR ..."
-		mkdir -p "$DATADIR"
-		mysql_install_db --skip-test-db --datadir="$DATADIR" > /dev/null
-
-		SOCKET="$(_get_config 'socket' "$@")"
-		"$@" --silent-startup --skip-networking --socket="${SOCKET}" &
+		echo "* Initializing ${DATADIR} ..."
+		mysql_install_db --cross-bootstrap --auth-root-authentication-method=normal --skip-test-db --datadir="${DATADIR}" > /dev/null
+		# Start ephemeral mysql server just to create the DB
+		"$@" --silent-startup --skip-networking --skip-name-resolve --skip-host-cache --skip-slave-start --socket="${SOCKET}" &
 		pid=$!
-
-		mysql=( mysql --protocol=socket -uroot -hlocalhost --socket="${SOCKET}" )
 		for i in {30..0}
 		do
 			if echo 'SELECT 1' | "${mysql[@]}" &> /dev/null
@@ -112,37 +105,33 @@ then
 			echo '* MySQL init process in progress ...'
 			sleep 1
 		done
-		if [ "$i" == 0 ]
+		if [ $i -eq 0 ]
 		then
 			echo >&2 '* MySQL init process FAILED.'
 			exit 1
 		fi
-
-		if [ -z "$MYSQL_INITDB_SKIP_TZINFO" ]
+		# TZINFO
+		if [ -z "${MYSQL_INITDB_SKIP_TZINFO}" ]
 		then
 			# sed is for https://bugs.mysql.com/bug.php?id=20545
 			mysql_tzinfo_to_sql /usr/share/zoneinfo | sed 's/Local time zone must be set--see zic manual page/FCTY/' | "${mysql[@]}" mysql
 		fi
-
-		if [ ! -z "$MYSQL_RANDOM_ROOT_PASSWORD" ]
-		then
-			export MYSQL_ROOT_PASSWORD="$(pwgen -1 32)"
-			echo "* Generated ROOT password: $MYSQL_ROOT_PASSWORD"
-		fi
-
 		rootgrants=""
 		# default root to listen for connections from anywhere
 		file_env 'MYSQL_ROOT_HOST' '%'
-		if [ ! -z "$MYSQL_ROOT_HOST" -a "$MYSQL_ROOT_HOST" != 'localhost' ]
+		if [ ! -z "${MYSQL_ROOT_HOST}" ] && [ "${MYSQL_ROOT_HOST}" != "localhost" ]
 		then
 			# no, we don't care if read finds a terminating character in this heredoc
 			# https://unix.stackexchange.com/questions/265149/why-is-set-o-errexit-breaking-this-read-heredoc-expression/265151#265151
 			read -r -d '' rootgrants <<-EOSQL || true
 				CREATE USER 'root'@'${MYSQL_ROOT_HOST}' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}' ;
 				GRANT ALL ON *.* TO 'root'@'${MYSQL_ROOT_HOST}' WITH GRANT OPTION ;
+				-- Root can login automatically via socket
+				-- INSTALL SONAME 'auth_socket';
+				-- GRANT ALL ON *.* TO 'root'@'localhost' IDENTIFIED VIA unix_socket WITH GRANT OPTION ;
 			EOSQL
 		fi
-
+		# Run
 		"${mysql[@]}" <<-EOSQL
 			-- What's done in this file shouldn't be replicated
 			--  or products like mysql-fabric won't work
@@ -154,30 +143,59 @@ then
 			DROP DATABASE IF EXISTS test ;
 			FLUSH PRIVILEGES ;
 		EOSQL
-
-		if [ ! -z "$MYSQL_ROOT_PASSWORD" ]
+		# Stop the ephemeral server
+		kill -s TERM ${pid}
+		if ! wait ${pid}
 		then
-			mysql+=( -p"${MYSQL_ROOT_PASSWORD}" )
+			echo >&2 '* MySQL init process has FAILED.'
+			exit 1
 		fi
-
-		file_env 'MYSQL_DATABASE'
-		if [ "$MYSQL_DATABASE" ]
+	fi	
+	MYSQL_ROOT_PASSWORD="$(<${DATADIR}/root.password)"
+	if [ ! -z "${MYSQL_ROOT_PASSWORD}" ]
+	then
+		mysql+=( -p"${MYSQL_ROOT_PASSWORD}" )
+	fi
+	file_env 'MYSQL_DATABASE'
+	if [ "${MYSQL_DATABASE}" ] && [ ! -d "${DATADIR}/${MYSQL_DATABASE}" ]
+	then
+		# Start ephemeral mysql server just to create the DB
+		"$@" --silent-startup --skip-networking --skip-name-resolve --skip-host-cache --skip-slave-start --socket="${SOCKET}" &
+		pid=$!
+		for i in {30..0}
+		do
+			if echo 'SELECT 1' | "${mysql[@]}" &> /dev/null
+			then
+				break
+			fi
+			echo "* MySQL DB '${MYSQL_DATABASE}' startup process running ..."
+			sleep 1
+		done
+		if [ $i -eq 0 ]
 		then
-			echo "* Generating database $MYSQL_DATABASE if not exists ..."
-			echo "CREATE DATABASE IF NOT EXISTS \`$MYSQL_DATABASE\` ;" | "${mysql[@]}"
-			mysql+=( "$MYSQL_DATABASE" )
+			echo >&2 "* MySQL DB '${MYSQL_DATABASE}' creation process has FAILED."
+			exit 1
 		fi
-
-		file_env 'MYSQL_USER'
-		file_env 'MYSQL_PASSWORD'
-		if [ "$MYSQL_USER" -a "$MYSQL_PASSWORD" ]
+		# Create the database. Do a check to see if the DB is there is faster
+		if [ ! -d "${DATADIR}/${MYSQL_DATABASE}" ]
 		then
-			echo "* Creating user $MYSQL_USER and grant access to database $MYSQL_DATABASE ..."
-			echo "CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD' ;" | "${mysql[@]}"
-			[ -n "$MYSQL_DATABASE" ] && echo "GRANT ALL ON \`$MYSQL_DATABASE\`.* TO '$MYSQL_USER'@'%' ;" | "${mysql[@]}"
-			echo 'FLUSH PRIVILEGES ;' | "${mysql[@]}"
+			echo "* Generating database '${MYSQL_DATABASE}' if not exists ..."
+			echo "CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\` CHARACTER SET utf8 COLLATE utf8_general_ci;" | "${mysql[@]}"
+			mysql+=( "${MYSQL_DATABASE}" )
+			# Set user and password
+			file_env 'MYSQL_USER'
+			file_env 'MYSQL_PASSWORD'
+			if [ -n "${MYSQL_USER}" ] && [ -n "${MYSQL_PASSWORD}" ]
+			then
+				echo "* Creating user ${MYSQL_USER} and grant access to database ${MYSQL_DATABASE} ..."
+				echo "CREATE USER '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}' ;" | "${mysql[@]}"
+				[ -n "${MYSQL_DATABASE}" ] && echo "GRANT ALL ON \`${MYSQL_DATABASE}\`.* TO '${MYSQL_USER}'@'%' ;" | "${mysql[@]}"
+				echo 'FLUSH PRIVILEGES ;' | "${mysql[@]}"
+			fi
+		else
+			mysql+=( "${MYSQL_DATABASE}" )
 		fi
-
+		# Load dumps or execute other files
 		for f in /docker-entrypoint-initdb.d/*
 		do
 			case "${f}" in
@@ -200,14 +218,14 @@ then
 				;;
 			esac
 		done
-
-		if ! kill -s TERM ${pid} || ! wait ${pid}
+		# Stop the ephemeral server
+		kill -s TERM ${pid}
+		if ! wait ${pid}
 		then
-			echo >&2 '* MySQL init process FAILED.'
+			echo >&2 "* MySQL DB '${MYSQL_DATABASE}' init process has FAILED."
 			exit 1
 		fi
-
-		echo '* MySQL init process DONE. Ready for start up.'
+		echo "* MySQL DB '${MYSQL_DATABASE}' init process is DONE. Ready for start up."
 		echo
 	fi
 fi
