632c94b639f577ad29e95a9419d32a592d64439a
Jose Riguera <jriguera@gmail.com>
2021-11-26 13:52:44 +0100

    update alpine and fix healthcheck

--- docker/healthcheck.sh
+++ docker/healthcheck.sh
@@ -5,13 +5,6 @@ host="${MYSQL_ROOT_HOST:-127.0.0.1}"
 user="${MYSQL_USER:-root}"
 pass="${MYSQL_PASSWORD:-${MYSQL_ROOT_PASSWORD:-$(<${DATADIR}/root.password)}}"
 
-args=(
-	# force mysql to not use the local "mysqld.sock" (test "external" connectibility)
-	-h"$host"
-	-u"$user"
-	--silent
-)
-
-mysqladmin "${args[@]}" ping > /dev/null && exit 0
+mysqladmin -h"$host" -u"$user" -p"$pass"  ping  && exit 0
 exit 1
 
